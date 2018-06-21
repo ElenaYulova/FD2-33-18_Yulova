38746e55baa1be9cc9eeb544e155b8123f8d11fa
ElenaYulova <elenavy@gmail.com>
2018-06-21 21:11:54 +0300

    N.13

--- HashStorage/HashStorage.js
+++ HashStorage/HashStorage.js
@@ -1,34 +1,31 @@
 "use strict"
-class HashStorage {
-    constructor()
-{        
+function HashStorage() {
+          
     this.storage = {};
     
 
-    this.addValue = function(key,value) {
-        var key = this.key;
-        var value = this.value;
+HashStorage.prototype.addValue = function(key,value) {
+        
         this.storage[key] = value;
         return this;
         
     }
-    this.getValue = function(key)  {
+    HashStorage.prototype.getValue = function(key)  {
         
-         this.key;
+        return this.storage[key];
     }
-    this.deleteValue = function(key)  {
-        var key = this.key;
-        if (key in this) {
-            delete this.key;
+    HashStorage.prototype.deleteValue = function(key)  {
+        if (key in this.storage) {
+            delete this.storage[key];
             return true;
         } else {
             return false;
         }
     }
-    this.getKeys = function()  {
-        return Object.keys(this);
+    HashStorage.prototype.getKeys = function()  {
+        return Object.keys(this.storage);
     }
-}
+
 }
 
 var drinkStorage = new HashStorage;
@@ -61,7 +58,54 @@ var drinkAddValue = function() {
         "рецепт приготовления: " : recipe
     };
     return drinkStorage.addValue(drink, descr);
+}
+
+var drinkValue = function() {
+    var i=0;
+    do {
+        var drink = prompt("Введите название напитка");
+        i++;
+    } while (typeof drink != "string" || i > 5);
+    var descr = drinkStorage.getValue(drink);
+    if (drink in drinkStorage.storage){
+        console.log("Информация о напитке:");
+        for (var key in descr) {
+            console.log(key + ": " + descr[key]);
+        }
+    } else {
+        console.log("Нет такого напитка");
     }
 
+}
 
-drinkAddValue();
+var deleteDrink = function () {
+    var i=0;
+    do {
+        var drink = prompt("Введите название напитка");
+        i++;
+    } while (typeof drink != "string" || i > 5);
+    if (drink in drinkStorage.storage) {
+        drinkStorage.deleteValue(drink);
+        console.log("Напиток успешно удалён");
+        
+    } else {
+        console.log("Нет такого напитка");
+    }
+    
+
+}
+var allDrinks = function () {
+    console.log("Перечень напитков: \n" + drinkStorage.getKeys());
+}
+
+drinkStorage.addValue("Кофе", {
+    "напиток: " : "Кофе",
+    "алкогольный напиток: " : "нет",
+    "рецепт приготовления: " : "Насыпать чайную ложку порошка кофе в турку, добавить сахар и специи по вкусу, залить водой, довести до кипения"
+});
+
+drinkStorage.addValue("Пиво", {
+    "напиток: " : "Пиво",
+    "алкогольный напиток: " : "да",
+    "рецепт приготовления: " : "Открыть бутылку, налить в бокал, пить с удовольствием"
+});
