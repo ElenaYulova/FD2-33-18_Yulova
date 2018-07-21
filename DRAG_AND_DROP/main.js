b75deb587237420a3a8af514f2cd71f7813cfdd0
Елена <39648351+ElenaYulova@users.noreply.github.com>
2018-07-21 17:56:13 +0300

    N.30 corrected

--- DRAG_AND_DROP/main.js
+++ DRAG_AND_DROP/main.js
@@ -9,28 +9,30 @@ for (var i = elems.length - 1; i >= 0; i--) {
   elem.style.top = elem.offsetTop+"px";
 }
 
-document.body.addEventListener('mousedown', dragging, false);
-
-function dragging(EO) {
+document.onmousedown = function dragging(EO) {
   EO = EO || window.event;
   var elem = EO.target;
   EO.preventDefault();  
   var shiftX = EO.pageX - elem.getBoundingClientRect().left;
   var shiftY = EO.pageY - elem.getBoundingClientRect().top;
-
+  
   document.body.appendChild(elem);
-
+  elem.style.cursor = 'pointer';
   document.onmousemove = function (EO) {
+    
     elem.style.left = EO.pageX - shiftX + "px";
     elem.style.top = EO.pageY - shiftY + "px";
   };
-
+  
   elem.onmouseup = function() {
       document.onmousemove = null;
+      elem.style.cursor = 'default';
     }
   
 
   elem.ondragstart = function () {
     return false;
   };
+  
+  
 }
